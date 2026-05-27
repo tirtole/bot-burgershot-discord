2db4db120b93d5fe4f51ccb0bfa02242964e574d
tirtole <164197660+tirtole@users.noreply.github.com>
2026-05-27 18:22:51 +0200

    Update database.py

--- Bot BurgerShot/database.py
+++ Bot BurgerShot/database.py
@@ -11,3 +11,16 @@ async def connect_db():
     )
 
     print("[DATABASE] Connecté à PostgreSQL")
+
+await db.execute("""
+CREATE TABLE IF NOT EXISTS services (
+    user_id BIGINT PRIMARY KEY,
+    start_timestamp BIGINT
+)
+""")
+
+await db.execute(
+    "INSERT INTO services VALUES($1, $2)",
+    user_id,
+    timestamp
+)
